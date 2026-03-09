5aaef6837fbac2a51a641b398ec0c33c23cd129f
JocsanCantera <jocsan.cantera@gmail.com>
2026-03-09 15:06:56 -0600

    feat: se agrego app.js

---
+++ app.js
@@ -0,0 +1,26 @@
+import { registrarDestino, mostrarItinerario } from "./viajes.js";
+
+const iniciarApp = () => {
+
+    registrarDestino("Paris", "2026-06-15", "Avión", {
+        alojamiento: "Hotel",
+        noches: 5,
+        personas: 2
+    });
+
+    registrarDestino("Londres", "2026-07-01", "Tren", {
+        alojamiento: "Airbnb",
+        noches: 4,
+        personas: 3
+    });
+
+    registrarDestino("Tokio", "2026-08-10", "Avión", {
+        alojamiento: "Hostal",
+        noches: 7,
+        personas: 1
+    });
+
+    mostrarItinerario();
+};
+
+iniciarApp();
